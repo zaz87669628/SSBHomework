cbe003d33b39516695fcb383ff25ecc1249c9aca
zaz87669628 <56588141+zaz87669628@users.noreply.github.com>
2021-03-05 03:21:40 +0900

    Add files via upload

---
+++ HOMEWORK1/WORK1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WORK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数");
+            string strA = Console.ReadLine();
+
+            Console.WriteLine("请输入第二个数");
+            string strB = Console.ReadLine();
+
+            Console.WriteLine("请输入运算符");
+            String strX = Console.ReadLine();
+
+            if (strA != "" && strB != "")
+            {
+                int intA = Convert.ToInt32(strA);
+                int intB = Convert.ToInt32(strB);
+                int intC = new int();
+                switch (strX)
+                {
+                    case "+":
+                        intC = intA + intB;
+                        break;
+
+                    case "-":
+                        intC = intA - intB;
+                        break;
+
+                    case "*":
+                        intC = intA * intB;
+                        break;
+
+                    case "/":
+                        intC = intA / intB;
+                        break;
+
+                    default:
+                        Console.WriteLine("不支持该运算符");
+                        break;
+                }
+                Console.WriteLine("计算的结果是" + intC);
+            }
+            else
+            {
+                Console.WriteLine("您输入了空值");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HOMEWORK1/WinFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strA = textBox1.Text;
+            string strB = textBox2.Text;
+            String strX = comboBox1.Text.ToString();
+
+            if (strA != "" && strB != "")
+            {
+                int intA = Convert.ToInt32(strA);
+                int intB = Convert.ToInt32(strB);
+                int intC = new int();
+                switch (strX)
+                {
+                    case "+":
+                        intC = intA + intB;
+                        break;
+
+                    case "-":
+                        intC = intA - intB;
+                        break;
+
+                    case "*":
+                        intC = intA * intB;
+                        break;
+
+                    case "/":
+                        intC = intA / intB;
+                        break;
+
+                    default:
+                        MessageBox.Show("不支持该运算符");
+                        break;
+                }
+                string strC=Convert.ToString(intC);
+                textBox3.Text = strC;
+            }
+            else
+            {
+                MessageBox.Show("您输入了空值");
+            }
+
+        }
+    }
+}
